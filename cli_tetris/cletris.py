first steps to better rotation. todo fix collision and out of bounds detection

cli_tetris/cletris.py
@@ -34,7 +34,7 @@ clean up code
         4) screen
 
 fix:
- when you rotate at the bottom the game crashes
+when you rotate at the bottom or too far left the game crashes
 """
 
 #list of pieces
@@ -89,11 +89,12 @@ def update(key):    # key press handling
     if key in ("q", "Q"):
         raise urwid.ExitMainLoop()
     if key == "left":
-        if xax>0: #check if in bounds
+        if xax+cletris_core.find_first_nonzero_left(piece)>0: #check if in bounds
             if not cletris_core.collision(board, cletris_core.move_left(current)): #check for collision
                 xax = xax-1
     if key == "right":
-        if xax+piece.shape[1] < width: #check if in bounds
+        if xax+cletris_core.find_first_nonzero_right(piece)+1<width:
+        #if xax+piece.shape[1] < width: #check if in bounds
             if not cletris_core.collision(board, cletris_core.move_right(current)):
                 xax = xax+1
     if key in ("r", "R"):
@@ -176,7 +177,10 @@ def refresh(_loop, _data):
 
         #draw piece
         current = np.zeros((height, width), dtype="int")
-        current[a:a+piece.shape[0], xax:xax+piece.shape[1]] = piece
+        #current[a:a+piece.shape[0], xax:xax+piece.shape[1]] = piece[cletris_core.find_first_nonzero_in_row(piece):]
+        for i, j in np.ndenumerate(piece):
+            if j != 0:
+                current[a+i[0], xax+i[1]] = j
 
         #check for collision:
         if cletris_core.collision(board, current):
@@ -201,7 +205,7 @@ def refresh(_loop, _data):
         if time.time()-timestep>inc_speed:
 
             #check if reached bottom or collision
-            if ((a+1)+piece.shape[0] > height) or cletris_core.collision(board, cletris_core.move_down(current)):
+            if ((a+1)+cletris_core.find_first_nonzero_down(piece)+1 > height) or cletris_core.collision(board, cletris_core.move_down(current)):
                 board = board + current # add piece to background
 
                 #increment piece

cli_tetris/cletris_core.py
@@ -70,6 +70,26 @@ def move_left(arr):
 def move_right(arr):
     return np.concatenate((np.rot90([arr[:, -1]], axes=(1, 0)), arr[:, :-1]), axis=1)
 
+def find_first_nonzero_left(tet):
+    min = 3
+    for row in tet[:]:
+        for i in range(len(row)):
+            if (row[i] != 0) and (i < min):
+                min = i
+    return min
+
+def find_first_nonzero_right(tet):
+    max = 0
+    for row in tet[:]:
+        for i in range(len(row)):
+            if (row[i] != 0) and (i > max):
+                max = i
+    return max
+
+def find_first_nonzero_down(tet):
+    return find_first_nonzero_right(np.rot90(tet))
+
+
 
 class tetromino():
     arr = None
@@ -77,36 +97,39 @@ class tetromino():
 
 
 class i_bar(tetromino):
-    arr = np.array([[1, 1, 1, 1]])
+    arr = np.array([[0, 0, 1, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 1, 0]])
 
 class t_bar(tetromino):
-    arr = np.array([[0, 2],
-                    [2, 2],
-                    [0, 2]])
+    arr = np.array([[0, 2, 0],
+                    [2, 2, 0],
+                    [0, 2, 0]])
 
 class o_bar(tetromino):
     arr = np.array([[3, 3],
                     [3, 3]])
 
 class j_bar(tetromino):
-    arr = np.array([[0, 4],
-                    [0, 4],
-                    [4, 4]])
+    arr = np.array([[0, 4, 0],
+                    [0, 4, 0],
+                    [4, 4, 0]])
 
 class l_bar(tetromino):
-    arr = np.array([[5, 0],
-                    [5, 0],
-                    [5, 5]])
+    arr = np.array([[0, 5, 0],
+                    [0, 5, 0],
+                    [0, 5, 5]])
 
 class z_bar(tetromino):
-    arr = np.array([[0, 6],
-                    [6, 6],
-                    [6, 0]])
+    arr = np.array([[0, 6, 0],
+                    [6, 6, 0],
+                    [6, 0, 0]])
 
 class s_bar(tetromino):
-    arr = np.array([[7, 0],
-                    [7, 7],
-                    [0, 7]])
+    arr = np.array([[0, 7, 0],
+                    [0, 7, 7],
+                    [0, 0, 7]])
 
 if __name__=="__main__":
     a = s_bar().arr